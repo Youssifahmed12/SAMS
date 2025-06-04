9a86fd82df5fa90630b774ef046f1dba30fbbf63
Youssifahmed12 <youssefbro12397@gmail.com>
2025-06-05 02:22:08 +0300

    Added Login DAL

---
+++ SAMSDataAccess/Users.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Configuration;
+
+namespace SAMSDataAccess
+{
+    public static class Users
+    {
+        public static bool Login(string username, string password)
+        { 
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["SAMSDB"].ConnectionString))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand("SELECT 1 FROM Users WHERE Username = @UserName AND Password = @Password LIMIT 1", conn))
+                {
+                    cmd.Parameters.AddWithValue("@UserName", username);
+                    cmd.Parameters.AddWithValue("@Password", password);
+                    return Convert.ToInt32(cmd.ExecuteScalar()) > 0;
+                }
+            }
+        }
+
+    }
+}
