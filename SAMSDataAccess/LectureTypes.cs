18e1a367423bcedbd5e9764f0277a2965211ee60
Youssifahmed12 <youssefbro12397@gmail.com>
2025-06-05 14:57:47 +0300

    Add Lectures/LectureTypes DAL

---
+++ SAMSDataAccess/LectureTypes.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Configuration;
+
+namespace SAMSDataAccess
+{
+    public static class LectureTypes
+    {
+        public static DataTable GetLectureTypes()
+        {
+            DataTable dt = new DataTable();
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["SAMSDB"].ConnectionString))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM LectureTypes", conn))
+                using (SQLiteDataAdapter da = new SQLiteDataAdapter(cmd))
+                {
+                    da.Fill(dt);
+                }
+            }
+            return dt;
+        }
+        public static bool AddLectureType(string Name)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["SAMSDB"].ConnectionString))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand("INSERT INTO LectureTypes (Name) VALUES (@Name)", conn))
+                {
+                    cmd.Parameters.AddWithValue("@Name", Name);
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+        public static bool UpdateLectureType(int TypeID, string Name)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["SAMSDB"].ConnectionString))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand("UPDATE LectureTypes SET Name = @Name WHERE TypeID = @TypeID", conn))
+                {
+                    cmd.Parameters.AddWithValue("@TypeID", TypeID);
+                    cmd.Parameters.AddWithValue("@Name", Name);
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+        public static bool DeleteLectureType(int TypeID)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["SAMSDB"].ConnectionString))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand("DELETE FROM LectureTypes WHERE TypeID = @TypeID", conn))
+                {
+                    cmd.Parameters.AddWithValue("@TypeID", TypeID);
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+    }
+}
---
+++ SAMSDataAccess/Lectures.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+using System.Configuration;
+using System.Data;
+
+namespace SAMSDataAccess
+{
+    public static class Lectures
+    {
+        public static bool AddLecture(int LecNum, int CourseID, int TypeID, DateTime date)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["SAMSDB"].ConnectionString))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand("INSERT INTO Lectures (LecNum, CourseID, TypeID, Date) VALUES (@LecNum, @CourseID, @TypeID, @Date)", conn))
+                {
+                    cmd.Parameters.AddWithValue("@LecNum", LecNum);
+                    cmd.Parameters.AddWithValue("@CourseID", CourseID);
+                    cmd.Parameters.AddWithValue("@TypeID", TypeID);
+                    cmd.Parameters.AddWithValue("@Date", date);
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+        public static bool DeleteLecture(int LecID)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["SAMSDB"].ConnectionString))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand("DELETE FROM Lectures WHERE LecID = @LecID", conn))
+                {
+                    cmd.Parameters.AddWithValue("@LecID", LecID);
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+        public static bool UpdateLecture(int LecID, int LecNum, int CourseID, int TypeID, DateTime date)
+        {
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["SAMSDB"].ConnectionString))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand("UPDATE Lectures SET LecNum = @LecNum, CourseID = @CourseID, TypeID = @TypeID, Date = @Date WHERE LecID = @LecID", conn))
+                {
+                    cmd.Parameters.AddWithValue("@LecID", LecID);
+                    cmd.Parameters.AddWithValue("@LecNum", LecNum);
+                    cmd.Parameters.AddWithValue("@CourseID", CourseID);
+                    cmd.Parameters.AddWithValue("@TypeID", TypeID);
+                    cmd.Parameters.AddWithValue("@Date", date);
+                    return cmd.ExecuteNonQuery() > 0;
+                }
+            }
+        }
+
+        public static DataTable GetLectures ()
+        {
+
+            DataTable dt = new DataTable();
+            using (SQLiteConnection conn = new SQLiteConnection(ConfigurationManager.ConnectionStrings["SAMSDB"].ConnectionString))
+            {
+                conn.Open();
+                using (SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM Lectures", conn))
+                using (SQLiteDataAdapter da = new SQLiteDataAdapter(cmd))
+                {
+                    da.Fill(dt);
+                }
+            }
+            return dt;
+        }
+
+    }
+}
