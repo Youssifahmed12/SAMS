358304ab7f4c9ee8b735840d498af7348840aec4
Youssifahmed12 <youssefbro12397@gmail.com>
2025-06-07 04:58:58 +0300

    Add Lectures / Lecture Types BLL

--- SAMSBuisness/Lectures/Lectures.cs
+++ SAMSBuisness/Lectures/Lectures.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 
 namespace SAMSBuisness.Lectures
 {
-    internal class Lectures
+    public class Lectures
     {
+        public static DataTable GetAllLectures()
+        {
+            return SAMSDataAccess.Lectures.GetLectures();
+        }
+        public static bool AddLecture(int LecNum, int CourseID, int TypeID, DateTime date)
+        {
+            return SAMSDataAccess.Lectures.AddLecture(LecNum, CourseID, TypeID, date);
+        }
+        public static bool UpdateLecture(int LecID, int LecNum, int CourseID, int TypeID, DateTime date)
+        {
+            return SAMSDataAccess.Lectures.UpdateLecture(LecID, LecNum, CourseID, TypeID, date);
+        }
+        public static bool DeleteLecture(int LecID)
+        {
+            return SAMSDataAccess.Lectures.DeleteLecture(LecID);
+        }
     }
 }
--- SAMSBuisness/Lectures/LecturesTypes.cs
+++ SAMSBuisness/Lectures/LecturesTypes.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SAMSBuisness.Lectures
 {
-    internal class LecturesTypes
+   public class LecturesTypes
     {
+        public static DataTable GetLectureTypes()
+        {
+            return SAMSDataAccess.LecturesTypes.GetLectureTypes();
+        }
+
+        public static bool AddLectureType(string Name)
+        {
+            return SAMSDataAccess.LecturesTypes.AddLectureType(Name);
+        }
+
+        public static bool UpdateLectureType(int TypeID,string Name)
+        {
+            return SAMSDataAccess.LecturesTypes.UpdateLectureType(TypeID,Name);
+        }
+
+        public static bool DeleteLectureType(int TypeID)
+        {
+            return SAMSDataAccess.LecturesTypes.DeleteLectureType(TypeID);
+        }
+
     }
 }
