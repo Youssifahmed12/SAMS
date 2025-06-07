e160e5c1f77a972a57adb4b46bf966f5393f8750
Youssifahmed12 <youssefbro12397@gmail.com>
2025-06-07 14:55:37 +0300

    Modify Lectures BLL According to Database Changes

--- SAMSBuisness/Lectures/Lectures.cs
+++ SAMSBuisness/Lectures/Lectures.cs
@@ -13,13 +13,15 @@ namespace SAMSBuisness.Lectures
         {
             return SAMSDataAccess.Lectures.GetLectures();
         }
-        public static bool AddLecture(int LecNum, int CourseID, int TypeID, DateTime date)
+        
+        public static bool AddLecture(int LecNum, int CourseID, int TypeID, DateTime date, float TeacherCut, float CenterCut)
         {
-            return SAMSDataAccess.Lectures.AddLecture(LecNum, CourseID, TypeID, date);
+            return SAMSDataAccess.Lectures.AddLecture(LecNum, CourseID, TypeID, date, TeacherCut, CenterCut);
         }
-        public static bool UpdateLecture(int LecID, int LecNum, int CourseID, int TypeID, DateTime date)
+
+        public static bool UpdateLecture(int LecID, int LecNum, int CourseID, int TypeID, DateTime date, float TeacherCut, float CenterCut)
         {
-            return SAMSDataAccess.Lectures.UpdateLecture(LecID, LecNum, CourseID, TypeID, date);
+            return SAMSDataAccess.Lectures.UpdateLecture(LecID, LecNum, CourseID, TypeID, date, TeacherCut, CenterCut);
         }
         public static bool DeleteLecture(int LecID)
         {
